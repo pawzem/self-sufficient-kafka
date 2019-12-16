2e571d40c8f2bafcb38ff698ee6a9f0e0bd27c1f
Paweł Żemła <pawzem@gmail.com>
2019-12-16 08:10:19 +0100

    Update run.ps1
    run container in background and restart unless stopped

--- run.ps1
+++ run.ps1
@@ -1,1 +1,1 @@
-docker run -p 2181:2181 -p 9092:9092 --name pawelkafka pawzem1994/self-sufficient-kafka
+docker run -d --restart=unless-stopped -p 2181:2181 -p 9092:9092 --name kafka pawzem1994/self-sufficient-kafka
